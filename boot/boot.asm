0079386a1c6e6fcdf0b47a45f645fb6eb670e96d
Kevin Alavik <kevin@alavik.se>
2024-03-14 11:58:49 +0000

    Wtf

--- boot/boot.asm
+++ boot/boot.asm
@@ -1,14 +1,8 @@
-section .data
-    text db 'Hello, World!', 0
-
 section .text
     global _start
-    extern clear_screen
     extern print_letter
-    extern print_string
 
 _start:
-    call clear_screen
-    mov ax, text
-    call print_string
+    mov al, 'A'
+    jmp print_letter
     hlt
--- boot/utils.asm
+++ boot/utils.asm
@@ -1,37 +1,8 @@
+
 section .text
     global print_letter
-    global clear_screen
-    global print_string
 
 print_letter:
     mov ah, 0x0e
-    mov bh, 0
-    mov bl, 0x07
-    int 10h
-    ret
-
-clear_screen:
-    cli
-    mov ah, 0
-    mov al, 3
-    int 10h
-
-    mov ah, 0x02
-    xor bh, bh
-    xor dh, dh
-    xor dl, dl
-    int 10h
-    ret
-
-[bits 16]
-print_string:
-    mov si, ax
-print_loop:
-    mov al, [si]
-    test al, al
-    jz print_done
-    call print_letter
-    inc si
-    jmp print_loop
-print_done:
+    int 0x10
     ret
