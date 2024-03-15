b910408d84b17e3f713a7636b7f71daddccbdb89
Kevin Alavik <kevin@alavik.se>
2024-03-15 10:07:20 +0000

    Some stuff

--- boot/boot.asm
+++ boot/boot.asm
@@ -8,10 +8,21 @@ section .text
     extern enable_protected
 
 _start:
+    ; Get kernel sector
+    mov ah, 0x02
+    mov al, 0x10
+    mov ax, 0
+    mov es, ax
+    mov di, 0x7E00
+    mov ch, 0
+    mov dh, 0
+    mov cl, 2
+    int 0x13
+
     call enable_a20
     call enable_gdt
     call enable_protected
     jmp load_kernel
  
 load_kernel:
-    hlt     ; Load kernel
+    jmp 0x7E00
