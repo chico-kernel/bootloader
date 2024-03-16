e9ad1f720d322f5f8d2d37abe2a1be2b63fa993f
Kevin Alavik <kevin@alavik.se>
2024-03-16 19:56:12 +0000

    Cleaned stuff up

--- boot/boot.asm
+++ boot/boot.asm
@@ -1,73 +1,77 @@
-[bits 16]
-[org 0x7c00]
+bits 16]
+section .data
+    gdt:
+        null:
+            dd 0x00000000
+            dd 0x00000000
+            db 0x00
+            db 0x00
+        kernel_code:
+            dd 0x0000FFFF
+            dd 0x00000000
+            db 0x9A
+            db 0xCF
+        kernel_data:
+            dd 0x0000FFFF
+            dd 0x00000000
+            db 0x92
+            db 0xCF
+        user_code:
+            dd 0x0000FFFF
+            dd 0x00000000
+            db 0xFA
+            db 0xCF
+        user_data:
+            dd 0x0000FFFF
+            dd 0x00000000
+            db 0xF2
+            db 0xCF
+        task_state:
+            dd 0x00000000       ; Replace with pointer to TSS
+            dd 0x0000           ; Replace with size of TSS
+            db 0x89
+            db 0x00
+
+gdt_end equ gdt + 7 * 8 ;
+
+section .data
+    gdtr:
+        dw 39
+        dd gdt
 
 section .text
-    global _start
+    global enable_protected
+    global enable_gdt
 
-_start:
-    ; Set video mode to mode 3 (80x25 text mode)
-    mov ah, 0x00
-    mov al, 0x03
-    int 0x10
+enable_protected:
+    mov eax, cr0
+    or eax, 0x1
+    mov cr0, eax
+    ret
 
-    mov ah, 0x0e
-    mov al, 'A'
-    int 0x10 
+enable_gdt:
+    call .set_gdt
+    call enable_protected
+    jmp 0x08:.reload_cs
+    ret
 
-    ; Set up the stack
-    mov ax, 0
-    mov ss, ax
-    mov sp, 0x7C00
-
-    mov ah, 0x0e
-    mov al, 'B'
-    int 0x10 
-
-    ; Enable A20
-    call enable_a20
-
-    mov ah, 0x0e
-    mov al, 'C'
-    int 0x10 
-
-    ; Get kernel sector
-    mov ax, 0
+.reload_cs:
+    mov ax, 0x10
+    mov ds, ax
     mov es, ax
-    mov ah, 0x02
-    mov al, 10
-    mov bx, 0x7E00
-    mov ch, 0
-    mov dh, 0
-    mov cl, 2
-    int 0x13
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+    ret
 
-    jc kernel_load_error
-
-    mov ah, 0x0e
-    mov al, 'D'
-    int 0x10 
-    
-    call enable_gdt
-    mov ah, 0x0e
-    mov al, 'E'
-    int 0x10 
-
-    jmp kernel_entry
-
-kernel_load_error:
-    mov ah, 0x0E
-    mov al, '!'
-    int 0x10
-    hlt
-
-kernel_entry:
-    mov ah, 0x0e
-    mov al, 'F'
-    int 0x10 
-    jmp 8:0x7E00
-    mov ah, 0x0e
-    mov al, 'G'
-    int 0x10
-    
-%include "boot/a20.asm"
-%include "boot/gdt.asm"
+.set_gdt:
+    xor eax, eax
+    mov ax, ds
+    shl eax, 4
+    add eax, gdt
+    mov [gdtr + 2], eax
+    mov eax, gdt_end
+    sub eax, gdt
+    mov [gdtr], ax
+    lgdt [gdtr]
+    ret
