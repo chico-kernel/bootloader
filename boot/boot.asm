91a92e545572a27385e3a2c2a086c34abb725ddc
Kevin Alavik <kevin@alavik.se>
2024-03-16 19:58:34 +0000

    â•Broken: Cleaned stuff up

--- boot/boot.asm
+++ boot/boot.asm
@@ -1,4 +1,113 @@
-bits 16]
+[bits 16]
+[org 0x7c00]
+
+section .text
+    global _start
+
+_start:
+    ; Set video mode to mode 3 (80x25 text mode)
+    mov ah, 0x00
+    mov al, 0x03
+    int 0x10
+
+    mov ah, 0x0e
+    mov al, 'A'
+    int 0x10
+
+    ; Set up the stack
+    mov ax, 0
+    mov ss, ax
+    mov sp, 0x7C00
+
+    mov ah, 0x0e
+    mov al, 'B'
+    int 0x10
+
+    ; Enable A20
+    call enable_a20
+
+    mov ah, 0x0e
+    mov al, 'C'
+    int 0x10
+
+    ; Get kernel sector
+    mov ax, 0
+    mov es, ax
+    mov ah, 0x02
+    mov al, 10
+    mov bx, 0x7E00
+    mov ch, 0
+    mov dh, 0
+    mov cl, 2
+    int 0x13
+
+    jc kernel_load_error
+
+    mov ah, 0x0e
+    mov al, 'D'
+    int 0x10
+
+    call enable_gdt
+    mov ah, 0x0e
+    mov al, 'E'
+    int 0x10
+
+    jmp kernel_entry
+
+kernel_load_error:
+    mov ah, 0x0E
+    mov al, '!'
+    int 0x10
+    hlt
+
+kernel_entry:
+    mov ah, 0x0e
+    mov al, 'F'
+    int 0x10
+    jmp 8:0x7E00
+    mov ah, 0x0e
+    mov al, 'G'
+    int 0x10
+
+enable_a20:
+    in al, 0x92
+    or al, 2
+    out 0x92, al
+    ret
+
+enable_protected:
+    mov eax, cr0
+    or eax, 0x1
+    mov cr0, eax
+    ret
+
+enable_gdt:
+    call .set_gdt
+    call enable_protected
+    jmp 0x08:.reload_cs
+    ret
+
+.reload_cs:
+    mov ax, 0x10
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+    ret
+
+.set_gdt:
+    xor eax, eax
+    mov ax, ds
+    shl eax, 4
+    add eax, gdt
+    mov [gdtr + 2], eax
+    mov eax, gdt_end
+    sub eax, gdt
+    mov [gdtr], ax
+    lgdt [gdtr]
+    ret
+
 section .data
     gdt:
         null:
@@ -38,40 +147,3 @@ section .data
     gdtr:
         dw 39
         dd gdt
-
-section .text
-    global enable_protected
-    global enable_gdt
-
-enable_protected:
-    mov eax, cr0
-    or eax, 0x1
-    mov cr0, eax
-    ret
-
-enable_gdt:
-    call .set_gdt
-    call enable_protected
-    jmp 0x08:.reload_cs
-    ret
-
-.reload_cs:
-    mov ax, 0x10
-    mov ds, ax
-    mov es, ax
-    mov fs, ax
-    mov gs, ax
-    mov ss, ax
-    ret
-
-.set_gdt:
-    xor eax, eax
-    mov ax, ds
-    shl eax, 4
-    add eax, gdt
-    mov [gdtr + 2], eax
-    mov eax, gdt_end
-    sub eax, gdt
-    mov [gdtr], ax
-    lgdt [gdtr]
-    ret
