a2c8e2c3c2f7b63decd3c024754772833580861e
Kevin Alavik <kevin@alavik.se>
2024-03-17 10:39:52 +0100

    Test

--- boot/boot.asm
+++ boot/boot.asm
@@ -29,6 +29,7 @@ mov cl, 2       ; Sector (2 = Kernel Sector)
 
 int 0x13
 jc disk_read_fail
+jmp load_kernel
 
 disk_read_fail:
     mov ah, 0x0E
@@ -89,6 +90,91 @@ disk_read_fail:
     mov al, '!'
     int 0x10
     hlt
+
+load_kernel:
+    dq 0x00
+
+    dw 0xFFFF
+    dw 0x0000
+    db 0x00
+    db 0b10011011
+    db 0b11001111
+    db 0x00
+    
+    dw 0xFFFF
+    dw 0x0000
+    db 0x00
+    db 0b10010011
+    db 0b11001111
+    db 0x00
+
+    dw 0x17
+    dd 0x7E00 ; Offset to where the kernel is loaded in memory
+
+    cli
+    lgdt [0x7E18]
+    mov eax, cr0
+    or eax, 1
+    mov cr0, eax
+    jmp 8:protected
+
+protected:
+    mov ax, 16
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+    mov ah, 0x0E
+    mov al, 'L'
+    int 0x10
+    mov ah, 0x0E
+    mov al, 'o'
+    int 0x10
+    mov ah, 0x0E
+    mov al, 'a'
+    int 0x10
+    mov ah, 0x0E
+    mov al, 'i'
+    int 0x10
+    mov ah, 0x0E
+    mov al, 'n'
+    int 0x10
+    mov ah, 0x0E
+    mov al, 'g'
+    int 0x10
+    mov ah, 0x0E
+    mov al, ' '
+    int 0x10
+    mov ah, 0x0E
+    mov al, 'K'
+    int 0x10
+    mov ah, 0x0E
+    mov al, 'e'
+    int 0x10
+    mov ah, 0x0E
+    mov al, 'r'
+    int 0x10
+    mov ah, 0x0E
+    mov al, 'n'
+    int 0x10
+    mov ah, 0x0E
+    mov al, 'e'
+    int 0x10
+    mov ah, 0x0E
+    mov al, 'l'
+    int 0x10
+    mov ah, 0x0E
+    mov al, '.'
+    int 0x10
+    mov ah, 0x0E
+    mov al, '.'
+    int 0x10
+    mov ah, 0x0E
+    mov al, '.'
+    int 0x10
+
+    jmp 0x7E00  ; Jump to kernel
 
 DISK:
     db 0        ; Define the disk number
